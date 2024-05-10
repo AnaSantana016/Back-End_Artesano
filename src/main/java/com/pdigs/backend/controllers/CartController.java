454b937af1e35b524f42a8e281384620f97b42d3
AlejandroOrtegaG <alejandroog99@gmail.com>
2024-05-10 12:37:27 +0100

    Product CRUD complete

--- src/main/java/com/pdigs/backend/controllers/CartController.java
+++ src/main/java/com/pdigs/backend/controllers/CartController.java
@@ -44,6 +44,36 @@ public class CartController {
         cartRepository.save(cart);
         return ResponseEntity.ok("Product added successfully to the sopping cart");
     }
+
+    @PutMapping(params = "product_id")
+    public ResponseEntity<String> updateProductAmount(@RequestBody Cart request, @RequestParam Integer product_id) {
+        if (request.getAmount() == 0){
+            deleteProductFromCart(request, product_id);
+            return ResponseEntity.ok("Product updated successfully to the sopping cart");
+        }
+        Iterable<Cart> userCarts= cartRepository.findCartsWithProductsByUserId(request.getUser().getId());
+
+        for (Cart cart : userCarts){
+            if (cart.getProduct().getId().equals(product_id)){
+                cart.setAmount(request.getAmount());
+                cartRepository.save(cart);
+            }
+        }
+        return ResponseEntity.ok("Product updated successfully in sopping cart");
+    }
+
+    @DeleteMapping(params = "product_id")
+    public ResponseEntity<String> deleteProductFromCart(@RequestBody Cart request, @RequestParam Integer product_id) {
+        Iterable<Cart> userCarts= cartRepository.findCartsWithProductsByUserId(request.getUser().getId());
+
+        for (Cart cart : userCarts){
+            if (cart.getProduct().getId().equals(product_id)){
+                cartRepository.delete(cart);
+            }
+        }
+        return ResponseEntity.ok("Product deleted successfully from sopping cart");
+    }
 }
+
 
 
